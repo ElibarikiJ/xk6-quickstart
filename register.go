b5198d2bee52116c3e795a8ec84dc8f3cefee9f3
Elibariki <brkjohn.3@gmail.com>
2026-05-12 23:57:31 +0300

    Refactor package and add SignMessage function
    Refactor package name and update module registration. Implement SignMessage method for signing messages using a PEM private key.

--- register.go
+++ register.go
@@ -1,9 +1,25 @@
-package example
+package quickstart
 
-import "go.k6.io/k6/v2/js/modules"
-
-const importPath = "k6/x/example"
+import (
+	"github.com/starkbank/ecdsa-go/v2/ellipticcurve/ecdsa"
+	"github.com/starkbank/ecdsa-go/v2/ellipticcurve/privatekey"
+	"go.k6.io/k6/js/modules"
+)
 
 func init() {
-	modules.Register(importPath, new(rootModule))
+	modules.Register("k6/x/starkbank", new(StarkBank))
+}
+
+type StarkBank struct{}
+
+// SignMessage takes a PEM private key and a message string, returning a Base64 signature
+func (s *StarkBank) SignMessage(pemKey string, message string) string {
+	// Import the key using Stark Bank's internal PEM parser
+	privKey := privatekey.FromPem(pemKey)
+	
+	// Generate a deterministic signature (RFC 6979)
+	signature := ecdsa.Sign(message, &privKey)
+	
+	// Return the Base64 DER encoded signature required by Stark Bank APIs
+	return signature.ToBase64()
 }
